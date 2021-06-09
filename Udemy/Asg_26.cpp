2074ed508cd1eeeb6fabb4d68015ba8ad3c9bd3f
Ramanarayanan Sambamurthy <srnarayanan1989@gmail.com>
2021-06-09 23:10:45 +0200

    Restored initial version of Asg_26

--- Udemy/Asg_26.cpp
+++ Udemy/Asg_26.cpp
@@ -1,31 +1,21 @@
 #include<iostream>
 
 void Asg_26() {
-	//Try to modify x1 & x2 and see the compilation output
-	int x = 5;
-	//int* y = &x;
-	int z ;
-	const int MAX = 12;
-	int& ref_x1 = x;
-	const int& ref_x2 = x;
+	#include<iostream>
 
-	//ref_x1 = 100;
-	//std::cout << " ref_x1= " << &ref_x1 << std::endl;
-	//std::cout << " x= " << &x << std::endl;
+	void Asg_26() {
+		//Try to modify x1 & x2 and see the compilation output	
+		int x = 5;
+		const int MAX = 12;
+		int& ref_x1 = x;
+		const int& ref_x2 = x;
 
-	x = 150;
-	std::cout << "ref_x2= " << ref_x2 << std::endl;
-	std::cout << " x= " << x << std::endl;
-	std::cout << "Addr ref_x2= " << &ref_x2 << std::endl; //
-	//std::cout << " Addr x= " << &x << std::endl;
-	//std::cout << " Addr y= " << &y << std::endl;
-	//y = y + 1;
-	//ref_x2 = x; // compile error. 
-	std::cout << "ref_x2= " << ref_x2 << std::endl;
-	std::cout << " x= " << x << std::endl;
-	std::cout << "Addr ref_x2= " << &ref_x2 << std::endl;
-	std::cout << " Addr z= " << &z << std::endl;
-	std::cout << " Addr x+1= " << (&x+1) << std::endl;
+		std::cout << "x value= " << x << std::endl;
+		std::cout << "ref_x1 value= " << ref_x1 << std::endl;
+		std::cout << "ref_x2 value= " << ref_x2 << std::endl;
+
+
+	}
 
 
 }
